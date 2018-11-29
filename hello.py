25357df86c7efb39c21a2e967c9cd69a40477aed
monawoh <39282031+monawoh@users.noreply.github.com>
2018-11-29 19:57:10 +0000

    basic setup for heroku

--- hello.py
+++ hello.py
@@ -1,27 +1,33 @@
-# from flask import Flask, render_template, request
-# app = Flask("MyApp")
-#
-# @app.route("/")
-# def hello():
-#     return "Hello World"
-#
-#
-# @app.route ("/<name>")
-# def hello_someone(name):
-#         return render_template("hello.html", name=name.title())
-#
-#
-#
-# app.run(debug=True)
-# FLASK_APP=hello.py flask run
-#  * Serving Flask app "hello"
-#  * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
-    #
-    # print 'hello world'
+from flask import Flask, render_template, request
+import request
+app = Flask("MyApp")
+
+def send_simple_message(email):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox9b397e46f7ab40d09d0d99d0adb827c6.mailgun.org/messages",
+        auth=("api", "d77a01c8ff4d9d31f456a6f9e59ca67f-9525e19d-4356b605"),
+        data={"from": "Excited User <mailgun@sandbox9b397e46f7ab40d09d0d99d0adb827c6.mailgun.org>",
+              "to": [email],
+              "subject": "Hello",
+              "text": "Testing some Mailgun awesomness!"})
 
 
+@app.route("/")
+def hello():
+    return "Hello World"
 
+@app.route("/<name>")
+def hello_someone(name):
+        return render_template("hello.html", name=name.title())
 
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    email = form_data["email"]
+    send_simple_message(email)
+    return "Email Sent to: {}".format(email)
+from os import environ
+app.run(host='0.0.0.0',debug=True, port=environ.get("PORT", 5000))
 
 #
 # import random
@@ -50,5 +56,5 @@ while roll_again == "yes" or roll_again == "y":
     print random.randint(min, max)
     print random.randint(min, max)
     print random.randint(min, max)
-    
+
     roll_again = raw_input("Roll the dices again?")
